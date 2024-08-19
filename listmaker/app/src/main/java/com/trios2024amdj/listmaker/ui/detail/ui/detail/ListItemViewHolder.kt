46a5efb756a1028527a75a3535bf41dcc4824392
Douglas Jasper <doug.jasper@trios.com>
2024-08-19 08:58:48 -0400

    Added Adapter and Holder

---
+++ listmaker/app/src/main/java/com/trios2024amdj/listmaker/ui/detail/ui/detail/ListItemViewHolder.kt
@@ -0,0 +1,9 @@
+package com.trios2024amdj.listmaker.ui.detail.ui.detail
+
+import androidx.recyclerview.widget.RecyclerView
+import com.trios2024amdj.listmaker.databinding.ListItemViewHolderBinding
+
+class ListItemViewHolder(val binding: ListItemViewHolderBinding) :
+    RecyclerView.ViewHolder(binding.root)
+{
+}
--- listmaker/app/src/main/java/com/trios2024amdj/listmaker/ui/detail/ui/detail/ListItemsRecyclerViewAdapter.kt
+++ listmaker/app/src/main/java/com/trios2024amdj/listmaker/ui/detail/ui/detail/ListItemsRecyclerViewAdapter.kt
@@ -1,4 +1,25 @@
 package com.trios2024amdj.listmaker.ui.detail.ui.detail
 
-class ListItemsRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.trios2024amdj.listmaker.databinding.ListItemViewHolderBinding
+import com.trios2024amdj.listmaker.models.TaskList
+
+class ListItemsRecyclerViewAdapter(var list: TaskList) :
+    RecyclerView.Adapter<ListItemViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        val binding = ListItemViewHolderBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return ListItemViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return list.tasks.size
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        holder.binding.textViewTask.text = list.tasks[position]
+    }
+
 }
